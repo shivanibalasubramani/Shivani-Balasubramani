55f614ff2199b16093868707995d955b02b5a4f2
Shivani Balasubramani <121674259+shivanibalasubramani@users.noreply.github.com>
2024-08-28 17:01:41 +0530

    Delete week4 2 directory

--- week4 2/BookstoreAPI/src/main/java/com/example/bookstoreapi/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bookstoreapi.repository;
-
-import com.example.bookstoreapi.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
